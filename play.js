b1e452efcc4be6e3cf2bb1131f7915bf9fa865dc
Michael Binnick <mbinnick@live.ca>
2022-08-31 18:02:53 +0000

    implemented exit game command (ctrl+c) using stdin callback

--- play.js
+++ play.js
@@ -3,3 +3,17 @@ const { connect } = require('./client.js');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function() {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
