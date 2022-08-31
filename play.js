17345067e8dd49d6ad1e936c95966d994dcbe9fe
Michael Binnick <mbinnick@live.ca>
2022-08-31 18:09:48 +0000

    refactored input function to it's own file and exported functionality

--- play.js
+++ play.js
@@ -1,19 +1,8 @@
 const net = require('net');
+const { setupInput } = require('./input');
 const { connect } = require('./client.js');
 
 console.log('Connecting ...');
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function() {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  }
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
+setupInput();
